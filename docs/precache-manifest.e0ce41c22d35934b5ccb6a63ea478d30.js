01026781e270e65c0dcc81c0d3f87959613cc911
Luan Nguyen <lihavim@gmail.com>
2020-03-26 17:13:11 +0700

    public to docs for github pages

---
+++ docs/precache-manifest.e0ce41c22d35934b5ccb6a63ea478d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d84883665ebc47afe4ab3a4ea303a4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a242b64219d91d9d46",
+    "url": "/static/css/main.1c67f61d.chunk.css"
+  },
+  {
+    "revision": "87abf9a49e4968da65cb",
+    "url": "/static/js/2.6993a0d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6993a0d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a242b64219d91d9d46",
+    "url": "/static/js/main.b79c9083.chunk.js"
+  },
+  {
+    "revision": "8b9f3700d8cae4c9bedb",
+    "url": "/static/js/runtime-main.17ad7960.js"
+  }
+]);
